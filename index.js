8bb0d6623457c97cd73b2b2c34b6b6d919e932b4
Luca Favini <luca.favini.02@gmail.com>
2025-03-11 14:45:52 +0100

    add move

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let touches = new Set();
 
 function start(e) {
     e.preventDefault();
-    console.log("START", e.touches)
     for (let i = 0; i < e.touches.length; i++) {
         let id = e.touches[i].identifier;
         if (touches.has(id)) {
@@ -11,23 +10,32 @@ function start(e) {
         touches.add(e.touches[i].identifier);
         addElement(e.touches[i].clientX, e.touches[i].clientY, e.touches[i].identifier);
     }
-    console.log(touches);
 }
 
 function end(e) {
     e.preventDefault();
-    console.log("END", e.touches)
     for (let i = 0; i < e.changedTouches.length; i++) {
         touches.delete(e.changedTouches[i].identifier);
 
-        console.log(e.changedTouches[i].identifier);
         let div = document.getElementById(e.changedTouches[i].identifier);
         while (!div) {
             div = document.getElementById(e.changedTouches[i].identifier);
         }
         div.remove();
     }
-    console.log(touches);
+}
+
+function move(e) {
+    e.preventDefault();
+    for (let i = 0; i < e.touches.length; i++) {
+        let id = e.touches[i].identifier;
+        let div = document.getElementById(id);
+        if (!div) {
+            continue
+        }
+        div.style.left = (e.touches[i].clientX - 50) + "px";
+        div.style.top = (e.touches[i].clientY - 50) + "px";
+    }
 }
 
 function addElement(x, y, id) {
