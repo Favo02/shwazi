8c12c9a46a51b5e57cdd74fb4872f89dabfb0fcb
Luca Favini <luca.favini.02@gmail.com>
2025-03-11 14:37:22 +0100

    working element wowwowow

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ function start(e) {
     e.preventDefault();
     console.log("START", e.touches)
     for (let i = 0; i < e.touches.length; i++) {
+        let id = e.touches[i].identifier;
+        if (touches.has(id)) {
+            continue;
+        }
         touches.add(e.touches[i].identifier);
+        addElement(e.touches[i].clientX, e.touches[i].clientY, e.touches[i].identifier);
     }
     console.log(touches);
 }
@@ -14,6 +19,22 @@ function end(e) {
     console.log("END", e.touches)
     for (let i = 0; i < e.changedTouches.length; i++) {
         touches.delete(e.changedTouches[i].identifier);
+
+        console.log(e.changedTouches[i].identifier);
+        let div = document.getElementById(e.changedTouches[i].identifier);
+        while (!div) {
+            div = document.getElementById(e.changedTouches[i].identifier);
+        }
+        div.remove();
     }
     console.log(touches);
+}
+
+function addElement(x, y, id) {
+    let element = document.createElement("div");
+    element.id = id;
+    element.classList.add("element");
+    element.style.left = (x - 50) + "px";
+    element.style.top = (y - 50) + "px";
+    document.body.appendChild(element);
 }
